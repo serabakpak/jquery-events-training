405e016504b69bd8791f916727dc2f2b24749203
Sera Bak <serabak15@gmail.com>
2016-08-18 19:58:04 -0700

    implements change function to add two numbers in change/base.js

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -3,5 +3,13 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   $("#time").text( Date.now() );
+  
 
+  $('.add').change(function(){
+  	var leftNum = $('#left').val();
+  	var rightNum = $('#right').val();
+  	if (leftNum !== "" && rightNum !== "") {
+  		$('#total').val(parseInt(leftNum) + parseInt(rightNum));
+  	}
+  });
 })
